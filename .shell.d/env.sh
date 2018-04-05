95e4496783b9f45266cb6aa2ab7793238ab9165c
Rami Al-Ghanmi <rami@ramialghanmi.com>
2018-04-04 23:40:12 -0700

    shell configuration

---
+++ .shell.d/env.sh
@@ -0,0 +1,15 @@
+# Path
+export PATH=$HOME/bin:$PATH
+
+
+# Editors
+export EDITOR=vim
+export GIT_EDITOR=$EDITOR
+export USE_EDITOR=$EDITOR
+export VISUAL=$EDITOR
+export PAGER=less
+
+# Terraform
+#export TF_LOG=DEBUG
+export TF_LOG_PATH="$HOME/.terraform.d/terraform.log"
+export TF_PLUGIN_CACHE_DIR="$HOME/.terraform.d/plugin-cache"
