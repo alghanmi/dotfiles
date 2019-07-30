ddead86db50f5090bd7bda64b368a4b041e9a504
Rami Al-Ghanmi <rami@ramialghanmi.com>
2019-07-30 14:39:40 -0700

    fix comment

--- .shell.d/go.sh
+++ .shell.d/go.sh
@@ -4,6 +4,6 @@
 export GOPATH="$HOME/go"
 export PATH=$GOPATH/bin:$PATH
 
-# GOROOT is now defined in ~/.shell.d/env-host.sh
+# GOROOT is now defined in ~/.shell.d/env-host-static.sh
 # This is done to reduce load time
 #export GOROOT="$(go env GOROOT)"
