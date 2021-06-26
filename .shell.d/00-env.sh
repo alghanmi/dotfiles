45794451b8f92ec674b8e0d21e12a6f07417555e
Rami AlGhanmi <rami@ramialghanmi.com>
2021-06-25 20:27:55 -0700

    Use rg instead of grep for FZF

--- .shell.d/00-env.sh
+++ .shell.d/00-env.sh
@@ -14,3 +14,6 @@ export LESS=-FRSX
 #export TF_LOG=DEBUG
 export TF_LOG_PATH="$HOME/.terraform.d/terraform.log"
 export TF_PLUGIN_CACHE_DIR="$HOME/.terraform.d/plugin-cache"
+
+# FZF
+FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow --glob "!.git/*"'
